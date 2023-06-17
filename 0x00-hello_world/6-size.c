0a0300edfad6be697a5e1731ef22e2d7530d3ffb
titem-012 <titemmifta@gmail.com>
2023-06-17 13:02:43 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+/**
+ * main - program prints size of various types on the computer its compiled on
+ * Return: 0 - Success
+ */
+int main(void)
+{
+printf("Size of a char: %u byte(s)\n", sizeof(char));
+printf("Size of an int: %u byte(s)\n", sizeof(int));
+printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+printf("Size of a float: %u byte(s)\n", sizeof(float));
+return (0);
+}
