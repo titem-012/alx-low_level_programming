4c02d8c8a8e93901955e2e2a69fc2daf4c89d804
titem-012 <titemmifta@gmail.com>
2023-06-17 16:22:41 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print digits from 0-9
+ * Return:return 0 (Success)
+*/
+int main(void)
+{
+int a;
+
+for (a = 0; a < 10; a++)
+{
+printf("%d", a);
+}
+printf("\n");
+return (0);
+}
