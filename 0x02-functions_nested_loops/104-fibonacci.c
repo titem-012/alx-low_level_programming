1a866b616a016533562a3fd2530993b2435d028d
titem-012 <titemmifta@gmail.com>
2023-06-20 19:42:07 -0700

    readme

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * numLength - returns the length of the string
+ * @num: operand number
+ * Return: number of digits
+ */
+
+int numLength(int num)
+{
+int length = 0;
+
+if (!num)
+return (1);
+while (num)
+{
+num = num / 10;
+length += 1;
+}
+return (length);
+}
+/**
+ * main - entry point
+ * Description: prints the first 98 fibonacci numbers
+ * strarting from 1 and 2 followed by a new line
+ * Return:0 (success)
+ */
+int main(void)
+{
+int count, initial;
+unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
+for (count = 1; count <= 98; count++)
+{
+if (f1o > 0)
+printf("%lu", f1o);
+initial = numLength(mx) - 1 - numLength(f1);
+while (f1o > 0 && initial > 0)
+{
+printf("%d", 0);
+initial--;
+}
+printf("%lu", f1);
+
+sum = (f1 + f2) % mx;
+sumo = f1o + f2o + (f1 + f2) / mx;
+f1 = f2;
+f1o = f2o;
+f2 = sum;
+f2o = sumo;
+
+if (count != 98)
+printf(", ");
+else
+printf("\n");
+}
+return (0);
+}
