1e2dd97ef62256afde6d4f87809480a95709e8e4
titem-012 <titemmifta@gmail.com>
2023-06-20 19:16:58 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Sum of a+b
+ */
+int add(int a, int b)
+{
+int sum = a + b;
+return (sum);
+}
