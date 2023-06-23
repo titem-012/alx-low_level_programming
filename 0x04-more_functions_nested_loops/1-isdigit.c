3071b5541f8cfee417331c884bd13fd0ceafa789
titem-012 <titemmifta@gmail.com>
2023-06-22 17:10:54 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - check if uppercase
+ * @c: Input
+ * Return: 1 if its uppercase,or else 0
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
