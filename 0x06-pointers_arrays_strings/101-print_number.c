887d04002f97638421057af79bb1305eb8ad91fe
titem-012 <titemmifta@gmail.com>
2023-06-29 21:07:19 -0700

    #

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number - print an integer
+ * @n: integer param
+ * Return: 0 (success)
+ */
+
+void print_number(int n)
+{
+unsigned int n1;
+n1 = n;
+
+if (n < 0)
+{
+_putchar('-');
+n1 = -n;
+}
+if (n1 / 10 != 0)
+{
+print_number(n1 / 10);
+}
+_putchar('0' + (n1 % 10));
+}
