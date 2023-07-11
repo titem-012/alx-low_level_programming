35313d298d9eabea9a3ccecd1a58a68e21fa3120
titem-012 <titemmifta@gmail.com>
2023-07-11 16:25:03 -0700

    %

--- 0x0B-malloc_free/1-main.c
+++ 0x0B-malloc_free/1-main.c
@@ -1,34 +1,23 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * *create_array - creates an array of chars,
- * and initializes it with a specific char
- * @size: size of the array to create
- * @c: char to initialize the array c
+ * main - check the code for ALX School students.
  *
- * Return: pointer to the array (Success), NULL (Error)
+ * Return: Always 0.
  */
-char *create_array(unsigned int size, char c)
+int main(void)
 {
-char *p;
-unsigned int i = 0;
+    char *s;
 
-if (size == 0)
-return (NULL);
-
-p = (char *)malloc(sizeof(char) * size);
-
-if (p == NULL)
-return (0);
-
-while (i < size)
-{
-*(p + i) = c;
-i++;
-}
-
-*(p + i) = '\0';
-
-return (p);
+    s = _strdup("ALX SE");
+    if (s == NULL)
+    {
+        printf("failed to allocate memory\n");
+        return (1);
+    }
+    printf("%s\n", s);
+    free(s);
+    return (0);
 }
