983dd142ccce7725eda02a48e14b9d039e65f7b9
titem-012 <titemmifta@gmail.com>
2023-07-10 17:16:02 -0700

    %

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * * _pow_recursion - function that returns the value of
+ *                      x rased to y
+ * @x: base
+ * @y: exponent
+ * Return: x to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+
+return (x * _pow_recursion(x, y - 1));
+}
