2818a52c12b41f14002b73501d59d990b736ee34
titem-012 <titemmifta@gmail.com>
2023-07-12 14:30:22 -0700

    %

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,3 +1,4 @@
+main.h
 
 #ifndef FILE_MAIN
 #define FILE_MAIN
@@ -6,38 +7,3 @@ int _putchar(char c);
 int _atoi(char *s);
 
 #endif
-
-_putchar.c
-
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-
-0-whatsmyname.c
-
-#include <stdio.h>
-#include "main.h"
-
-/**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
- */
-int main(int argc __attribute__((unused)), char *argv[])
-{
-printf("%s\n", *argv);
-
-return (0);
-}
