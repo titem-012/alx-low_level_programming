c2ebb8bb711d22da497fc00f91dcb3c909e6abbf
titem-012 <titemmifta@gmail.com>
2023-07-17 16:46:44 -0700

    %

---
+++ 0x0E-structures_typedef/5-main.c
@@ -0,0 +1,25 @@
+ many legs does a dog have if you call his tail a leg? Four. Saying that a tail is a leg doesn't make it a leg
+mandatory
+
+
+Write a function that frees dogs.
+
+Prototype: void free_dog(dog_t *d);
+julien@ubuntu:~/0x0d. structures, typedef$ cat 5-main.c
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    dog_t *my_dog;
+
+    my_dog = new_dog("Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+    free_dog(my_dog);
+    return (0);
+}
