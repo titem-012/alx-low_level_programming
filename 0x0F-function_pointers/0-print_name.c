c00490fdf1492f592b65405bfac4254334552e59
titem-012 <titemmifta@gmail.com>
2023-07-19 16:57:08 -0700

    %

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,12 +3,13 @@
 
 /**
 * print_name - prints a name
+*
 * @name: name to print
 * @f: pointer to the printing function
 */
 void print_name(char *name, void (*f)(char *))
 {
-if (!name || !f)
+if (name && f)
 	return;
 f(name);
 }
